f7311704ab8614877f4c8ab98a9ba20f345acf00
kkwatinetz <142681281+kkwatinetz@users.noreply.github.com>
2024-09-09 16:51:31 -0400

    Update HW2.c

--- HW2.c
+++ HW2.c
@@ -7,196 +7,146 @@ int main()
 {
     float hw, project, lab, midterm1, midterm2, Final;
     char startOver;
-    int counter = 0;
+    int counter;
     float finalScore;
     char letterGrade;
 
     do
     {
+        // HW average
+        counter = 0;
         printf("Enter the hw average score: ");
         scanf("%f", &hw);
-        while ((hw > 100 || hw < 0) && counter < 2)
+        while ((hw < 0 || hw > 100) && counter < 2)
         {
             counter++;
             printf("Invalid score. Try again: ");
             scanf("%f", &hw);
-            if (counter == 2)
-            {
-                printf("I'm sorry... You tried too many times.\nWould you like to start over (Y/N): ");
-                scanf(" %c", &startOver);
+        }
+        if (counter == 2)
+        {
+            printf("I'm sorry... You tried too many times.\nWould you like to start over (Y/N): ");
+            scanf(" %c", &startOver);
+            if (startOver != 'Y' && startOver != 'y')
                 break;
-            }
         }
 
+        // Project average
         counter = 0;
         printf("Enter the project average score: ");
         scanf("%f", &project);
-        while ((project > 100 || project < 0) && counter < 2)
+        while ((project < 0 || project > 100) && counter < 2)
         {
             counter++;
             printf("Invalid score. Try again: ");
             scanf("%f", &project);
-            if (counter == 2)
-            {
-                printf("I'm sorry... You tried too many times.\nWould you like to start over (Y/N): ");
-                scanf(" %c", &startOver);
+        }
+        if (counter == 2)
+        {
+            printf("I'm sorry... You tried too many times.\nWould you like to start over (Y/N): ");
+            scanf(" %c", &startOver);
+            if (startOver != 'Y' && startOver != 'y')
                 break;
-            }
         }
 
+        // Lab average
         counter = 0;
         printf("Enter lab average score: ");
         scanf("%f", &lab);
-        while ((lab > 100 || lab < 0) && counter < 3)
+        while ((lab < 0 || lab > 100) && counter < 2)
         {
             counter++;
             printf("Invalid score. Try again: ");
             scanf("%f", &lab);
-            if (counter == 2)
-            {
-                printf("I'm sorry... You tried too many times.\nWould you like to start over (Y/N): ");
-                scanf(" %c", &startOver);
+        }
+        if (counter == 2)
+        {
+            printf("I'm sorry... You tried too many times.\nWould you like to start over (Y/N): ");
+            scanf(" %c", &startOver);
+            if (startOver != 'Y' && startOver != 'y')
                 break;
-            }
+            continue;
         }
 
+        // Midterm1
         counter = 0;
         printf("Enter midterm1, midterm2, and final: ");
         scanf("%f %f %f", &midterm1, &midterm2, &Final);
-        while ((midterm1 > 100 || midterm1 < 0) && counter < 2)
+
+        // Validate midterm1
+        while ((midterm1 < 0 || midterm1 > 100) && counter < 2)
         {
             counter++;
             printf("Invalid midterm1 score. Try again: ");
             scanf("%f", &midterm1);
-            if (counter == 2)
-            {
-                printf("I'm sorry... You tried too many times.\nWould you like to start over (Y/N): ");
-                scanf(" %c", &startOver);
+        }
+        if (counter == 2)
+        {
+            printf("I'm sorry... You tried too many times.\nWould you like to start over (Y/N): ");
+            scanf(" %c", &startOver);
+            if (startOver != 'Y' && startOver != 'y')
                 break;
-            }
+            continue;
         }
 
+        // midterm2
         counter = 0;
-        while ((midterm2 > 100 || midterm2 < 0) && counter < 2)
+        while ((midterm2 < 0 || midterm2 > 100) && counter < 2)
         {
             counter++;
             printf("Invalid midterm2 score. Try again: ");
             scanf("%f", &midterm2);
-            if (counter == 2)
-            {
-                printf("I'm sorry... You tried too many times.\nWould you like to start over (Y/N): ");
-                scanf(" %c", &startOver);
-
+        }
+        if (counter == 2)
+        {
+            printf("I'm sorry... You tried too many times.\nWould you like to start over (Y/N): ");
+            scanf(" %c", &startOver);
+            if (startOver != 'Y' && startOver != 'y')
                 break;
-            }
+            continue;
         }
 
+        // Final
         counter = 0;
-        while ((Final > 100 || Final < 0) && counter < 2)
+        while ((Final < 0 || Final > 100) && counter < 2)
         {
             counter++;
             printf("Invalid final score. Try again: ");
             scanf("%f", &Final);
-            if (counter == 2)
-            {
-                printf("I'm sorry... You tried too many times.\nWould you like to start over (Y/N): ");
-                scanf(" %c", &startOver);
-                break;
-            }
         }
-
-        // Calculate the final score
-        finalScore = (hw * 0.2) + (project * 0.2) + (lab * 0.05) + ((midterm1 + midterm2 + Final) / 3) * 0.55;
-
-        // Determine letter grade
-        switch ((int)finalScore / 10)
+        if (counter == 2)
         {
-            case 10:
-            case 9:
-                letterGrade = 'A';
+            printf("I'm sorry... You tried too many times.\nWould you like to start over (Y/N): ");
+            scanf(" %c", &startOver);
+            if (startOver != 'Y' && startOver != 'y')
                 break;
-            case 8:
-                letterGrade = 'B';
-                break;
-            case 7:
-                letterGrade = 'C';
-                break;
-            case 6:
-                letterGrade = 'D';
-                break;
-            default:
-                letterGrade = 'F';
+            continue;
         }
-        printf("Here are all your average scores:\n");
-        printf("HW \t\t\t : %.2f\n",hw);
-        printf("PROJECTS \t\t\t : %.2f\n",project);
-        printf("LABS\t\t\t : %.2f\n",lab);
-        printf("MIDTERM1\t\t\t : %.2f\n",midterm1);
-        printf("MIDTERM2\t\t\t : %.2f\n",midterm2);
-        printf("FINAL\t\t\t : %.2f\n",Final);
 
-        printf("-------------------------------\n");
-        printf("OVERALL AVERAGE: %.2f\n", finalScore);
-        printf("(%.2f*.2 + %.2f*.2 + %.2f*.05 + (%.2f+%.2f+%.2f)*.55/3 is %.2f)",hw,project,lab,midterm1,midterm2,Final);
-        printf("-------------------------------\n");
-        printf("\t\t\tFINAL LETTER GRADE: %c\n", letterGrade);
 
-        // Ask if they want to start over
+        // Start Over?
         printf("Would you like to start over (Y/N): ");
         scanf(" %c", &startOver);
 
+    } while (startOver == 'Y' || startOver == 'y');
 
-    }
-    while (startOver == 'Y' || startOver == 'y');
-
+    //Make sure answer is No
     counter = 0;
-    while(startOver != 'N' || startOver != 'n')
+    while (startOver != 'N' && startOver != 'n' && counter < 2)
     {
-      counter++;
-      printf("Invalid character. Try again: ");
-      scanf("%c", &startOver);
-      if (counter == 2)
-      {
-            printf("\t\t\tProgram terminated!\n");
-      }
-
+        counter++;
+        printf("Invalid character. Try again: ");
+        scanf(" %c", &startOver);
     }
+    if (counter == 2)
+    {
+        printf("\t\t\tProgram terminated!\n");
+    }
+
 
     printf("\t\t\tGoodbye!\n");
 
     return 0;
 }
 
-
- /*
-    Enter the hw average score:
-    Invalid score. Try again:
-    Enter the project average score:
-    Invalid score. Try again:
-    Enter lab average score:
-    Invalid score. Try again:
-    Enter midterm1, midterm2 and the final:
-    Invalid ---. Try again:
-    Here are all your average scores:
-    HW          : 75.50
-    PROJECTS    : 63.20
-    LABS        : 100.00
-    MIDTERM1    : 78.00
-    MIDTERM2    : 90.00
-    FINAL       : 63.00
-    ------------------------------------
-    OVERALL AVERAGE : 75.09
-    (75.50*.2 + 63.20*.2 + 100.00*.05 + (78.00+90.00+63.00)*.55/3 is 75.09)
-    ------------------------------------
-    FINAL LETTER GRADE: C
-    Would you like to start over (Y/N):
-    *************************************
-    Good-bye!
-
-    If yes - start over from top
-    If score or letter incorrectly entered - give 3 tries then terminate run (float between 0-100 or Y,y,n,N)
-    All grades between 0-100
-    All average scores need to be declared as floats
-    *************************************
-    */
 
